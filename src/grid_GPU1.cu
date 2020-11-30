f8e03d688ee398be0c7f7521471228a852f4acf4
Marcin Rogowski <marcin.rogowski@kaust.edu.sa>
2020-11-30 23:19:53 +0300

    Templates for fill sine and linear

--- src/grid_GPU1.cu
+++ src/grid_GPU1.cu
@@ -48,7 +48,6 @@ __global__ void print_from_gpu(void)
 
 //-------------------------------------------------------------------------------------------------------
 
-// first (wrong) implementation of filling data(0:n1*n2*n3)=val
 // there is probably an easier way to implement this (3d blocks?)
 __global__ void cuda_fill_const(Myfloat *data, Myfloat val, int n1, int n2, int n3, Myint64 i1Start, Myint64 i1End, Myint64 i2Start, Myint64 i2End, Myint64 i3Start, Myint64 i3End)
 {
@@ -85,9 +84,81 @@ __global__ void cuda_fill_const(Myfloat *data, Myfloat val, int n1, int n2, int 
 
 //-------------------------------------------------------------------------------------------------------
 
+__global__ void cuda_fill_sine(Myfloat *data, Myfloat64 param1, Myfloat64 param2, Myfloat64 param3, Myfloat64 amp, int n1, int n2, int n3, Myint64 i1Start, Myint64 i1End, Myint64 i2Start, Myint64 i2End, Myint64 i3Start, Myint64 i3End)
+{
+	printf("sine %f %f %f %f\n",param1,param2,param3,amp);
+	Myfloat val = param1;
 
+	int size = n1*n2*n3;
+	int tid = threadIdx.x + blockIdx.x*blockDim.x;
 
+	// 1D to 3D index (thanks StackOverflow)
+	// public int[] to3D( int idx ) {
+	// 	final int z = idx / (xMax * yMax);
+	// 	idx -= (z * xMax * yMax);
+	// 	final int y = idx / xMax;
+	// 	final int x = idx % xMax;
+	// 	return new int[]{ x, y, z };
+	// }
 
+	while (tid < size)
+	{
+		int t_i3 = tid / (n1*n2);
+		int idx = tid-t_i3*n1*n2;
+		int t_i2 = idx/n1;
+		int t_i1 = idx%n1;
+
+		if (t_i1 >= i1Start && t_i1 <= i1End &&
+			t_i2 >= i2Start && t_i2 <= i2End &&
+			t_i3 >= i3Start && t_i3 <= i3End   )
+		{
+			data[tid] = val;
+			//printf("data[%d]=%f\n",tid,val);
+		}
+
+		tid += blockDim.x * gridDim.x;
+	}
+}
+
+//-------------------------------------------------------------------------------------------------------
+
+__global__ void cuda_fill_linear(Myfloat *data, Myfloat64 param1, Myfloat64 param2, Myfloat64 param3, Myfloat64 amp, int n1, int n2, int n3, Myint64 i1Start, Myint64 i1End, Myint64 i2Start, Myint64 i2End, Myint64 i3Start, Myint64 i3End)
+{
+	printf("linear %f %f %f %f\n",param1,param2,param3,amp);
+	Myfloat val = param1;
+
+	int size = n1*n2*n3;
+	int tid = threadIdx.x + blockIdx.x*blockDim.x;
+
+	// 1D to 3D index (thanks StackOverflow)
+	// public int[] to3D( int idx ) {
+	// 	final int z = idx / (xMax * yMax);
+	// 	idx -= (z * xMax * yMax);
+	// 	final int y = idx / xMax;
+	// 	final int x = idx % xMax;
+	// 	return new int[]{ x, y, z };
+	// }
+
+	while (tid < size)
+	{
+		int t_i3 = tid / (n1*n2);
+		int idx = tid-t_i3*n1*n2;
+		int t_i2 = idx/n1;
+		int t_i1 = idx%n1;
+
+		if (t_i1 >= i1Start && t_i1 <= i1End &&
+			t_i2 >= i2Start && t_i2 <= i2End &&
+			t_i3 >= i3Start && t_i3 <= i3End   )
+		{
+			data[tid] = val;
+			//printf("data[%d]=%f\n",tid,val);
+		}
+
+		tid += blockDim.x * gridDim.x;
+	}
+}
+
+//-------------------------------------------------------------------------------------------------------
 
 
 
@@ -223,8 +294,25 @@ void Grid_GPU1::fill(Point_type pType, Func_type t1,  Func_type t2, Func_type t3
 {
 	printDebug(FULL_DEBUG, "In Grid_GPU1::fill") ;
 
-	// TO DO
 	Grid::fill(pType, t1,  t2, t3, param1, param2, param3, amp) ;
+
+
+	//pointType
+	Myint64 i1Start, i1End, i2Start, i2End, i3Start, i3End ;
+	Grid::getGridIndex(INNER_POINTS, &i1Start, &i1End, &i2Start, &i2End, &i3Start, &i3End);
+
+	// we only use sine and linear for now
+	int ok = 1;
+	if (t1 != FUNC_SINE && t1 != FUNC_LINEAR) ok = 0;
+	if (t2 != FUNC_SINE && t2 != FUNC_LINEAR) ok = 0;
+	if (t3 != FUNC_SINE && t3 != FUNC_LINEAR) ok = 0;
+	if (!ok) printError("CUDA: only FUNC_SINE and FUNC_LINEAR implemented");
+
+	if ((t1==t2 && t2==t3)==false) printError("CUDA: func has to be same in each dimension");
+
+	if (t1 == FUNC_SINE) cuda_fill_sine<<<512,64>>>  (d_grid_3d,param1,param2,param3,amp,n1,n2,n3,i1Start,i1End,i2Start,i2End,i3Start,i3End);
+	else                 cuda_fill_linear<<<512,64>>>(d_grid_3d,param1,param2,param3,amp,n1,n2,n3,i1Start,i1End,i2Start,i2End,i3Start,i3End);
+
 
 	printDebug(FULL_DEBUG, "Out Grid_GPU1::fill") ;
 }
