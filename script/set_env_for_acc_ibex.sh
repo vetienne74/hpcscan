f9eb03b4b5e2a7c1cd16e46c2b5aa04d1ab9f1c5
Suha Kayum <x_kayumsn@gpu609-04.ibex.kaust.edu.sa>
2020-12-01 18:23:40 +0300

    acc modules

---
+++ script/set_env_for_acc_ibex.sh
@@ -0,0 +1,17 @@
+
+# environment for Ibex
+
+module load pgi/20.1
+module load cuda/11.0.1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sw/csgv/pgi/20.1/el7.7_binary/linux86-64/20.1/mpi/openmpi-3.1.3/lib
+export PATH=/sw/csgv/pgi/20.1/el7.7_binary/linux86-64/20.1/mpi/openmpi-3.1.3/bin:$PATH
+
+export HPCSCAN_MPI_INVOKER="srun"
+export HPCSCAN_NTHREADS=32
+
+alias sq='squeue -u x_etiennv'
+
+export KMP_AFFINITY=scatter,1,0,granularity=fine
+export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
+
+echo My environment is set for Ibex acc!
