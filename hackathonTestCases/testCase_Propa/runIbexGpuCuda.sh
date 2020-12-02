2dce7faefbe8d91c8129e7e83d7bde842d7ce9cd
Marcin Rogowski <marcin.rogowski@kaust.edu.sa>
2020-12-02 09:48:53 +0300

    scripts to run GpuCuda

---
+++ hackathonTestCases/testCase_Propa/runIbexGpuCuda.sh
@@ -0,0 +1,50 @@
+
+export KMP_AFFINITY=scatter,1,0,granularity=fine
+export OMP_NUM_THREADS=32
+
+start_time=$(date)
+
+paramAnalysisFile=runIbexGpuCuda.out
+rm -f ${paramAnalysisFile}
+
+if [ -z "$1" ]
+then
+    list_file=configIbexGpuCuda*.sh
+else
+    list_file=$1
+fi
+
+# if not specified, use mpirun to launch MPI
+if [ -z $HPCSCAN_MPI_INVOKER ]
+then
+    export HPCSCAN_MPI_INVOKER=mpirun
+fi
+echo Launch MPI with $HPCSCAN_MPI_INVOKER
+
+# clean dir
+sh clean_dir.sh
+
+# build scripts
+python3 buildConfigScriptIbexGpuCuda.py
+
+# run hpcscan
+# loop on all script files present in the directory
+for scriptFile in $( ls ${list_file}); do
+
+    #sh clean.sh
+
+    echo '* Run hpcscan with' ${scriptFile}
+    rm -f hpcscan.perf.Propa.log
+    sh ${scriptFile} > tmp
+    cat hpcscan.perf.Propa.log >> ${paramAnalysisFile}
+
+done
+
+end_time=$(date)
+tester=$(whoami)
+machine=$(hostname)
+
+echo "# Started  : " $start_time
+echo "# Ended    : " $end_time
+echo "On machine : " $machine
+echo "Done by    : " $tester
