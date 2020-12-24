aa64a3c8b63a1417f4a44bc1f5029e72f28c027e
vincent <vetienne@rocketmail.com>
2020-12-24 05:43:53 +0300

    update test cases scripts for Shaheen

---
+++ script/testCase_FD_D2/hpcscanFD_D2.sh
@@ -0,0 +1,34 @@
+
+# grid size
+n1=1000
+n2=1000
+n3=1000
+
+export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
+export KMP_AFFINITY=scatter,1,0,granularity=fine
+
+start_time=$(date)
+
+sh ../clean_dir.sh
+
+# FD_D2 (Baseline)
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 2
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 4
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 8
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 12
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 16
+
+# FD_D2_CacheBlk
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2_CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 2
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2_CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 4
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2_CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 8
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2_CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 12
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2_CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 16
+
+end_time=$(date)
+
+echo '*** TEST ENDED ***'
+echo "# Started  : " $start_time 
+echo "# Ended    : " $end_time
+echo "On machine : " `hostname`
+echo "Done by    : " `whoami`
