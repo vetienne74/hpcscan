4ebf0a831b0324c2686de899428c58d7b451ce2b
Vincent Etienne <vincent.etienne@aramco.com>
2020-12-25 09:16:08 +0300

    update results from Shaheen

--- script/testCase_Propa/strongWeakScalability/hpcscanPropaStrongWeakScalability.sh
+++ script/testCase_Propa/strongWeakScalability/hpcscanPropaStrongWeakScalability.sh
@@ -11,35 +11,35 @@ fdOrder=8
 
 $HPCSCAN_MPI_INVOKER -n 1 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 1 -nsub3 1
 
 $HPCSCAN_MPI_INVOKER -n 2 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10  \
 			-nsub1 1 -nsub2 2 -nsub3 1
 
 $HPCSCAN_MPI_INVOKER -n 4 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 2 -nsub3 2
 
 $HPCSCAN_MPI_INVOKER -n 6 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 3 -nsub3 2
 
 $HPCSCAN_MPI_INVOKER -n 8 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 4 -nsub3 2
@@ -49,35 +49,35 @@ fdOrder=8
 
 $HPCSCAN_MPI_INVOKER -n 1 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 1 -nsub3 1
 
 $HPCSCAN_MPI_INVOKER -n 2 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 2000 -n3 1000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10  \
 			-nsub1 1 -nsub2 2 -nsub3 1
 
 $HPCSCAN_MPI_INVOKER -n 4 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 2000 -n3 2000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 2 -nsub3 2
 
 $HPCSCAN_MPI_INVOKER -n 6 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 3000 -n3 2000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 3 -nsub3 2
 
 $HPCSCAN_MPI_INVOKER -n 8 ../../../bin/hpcscan \
 			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
-			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-dim 3 -nt 100 -dt 0.0 -ratioCFL 1.0 \
 			-fdOrder ${fdOrder} -n1 1000 -n2 4000 -n3 2000 \
 			-param1 130 -param2 130 -param3 130 -snapInc 10 \
 			-nsub1 1 -nsub2 4 -nsub3 2
