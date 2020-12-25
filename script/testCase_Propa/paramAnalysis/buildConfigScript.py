be11cbf11d87e3ed790114c02fb8eacf9c8ea181
vincent <vetienne@rocketmail.com>
2020-12-25 08:13:50 +0300

    update script/testCase_Propa

---
+++ script/testCase_Propa/paramAnalysis/buildConfigScript.py
@@ -0,0 +1,66 @@
+
+script_template = 'templateScript.sh'
+
+n1Range=['500','600','700','800','900','1000']
+#n1Range=['500','600','700']
+#fdOrderRange=['4','8']
+fdOrderRange=['4','8','12']
+#tmax='2.0'
+tmax='0.1'
+snapDt='0.01'
+dt='0.0'
+nmode='130'
+#dim='3'
+dim='1'
+# number of tries
+ntry='3'
+ratioCFLRange=['1.0','0.5','0.1']
+testModeRange=['CacheBlk']
+propagatorRange=['Ac2Standard','Ac2SplitComp']
+
+#-----------------------------------------------------------------
+configName='config'
+ii = 1
+for propagator in propagatorRange:
+    for testMode in testModeRange:
+        for n1 in n1Range:
+            for fdOrder in fdOrderRange:
+                for ratioCFL in ratioCFLRange:
+
+                    # create new script
+                    new_name = configName+str(ii)
+                    script_new = script_template.replace('template', new_name)                
+
+                    # skip some unwanted config
+                    if propagator == 'Ac2Standard' and testMode == 'NEC_SCA':
+                        continue
+
+                    # open files
+                    f2 = open(script_new, 'w')
+                    f1 = open(script_template, 'r')
+
+                    # create new file
+                    for line in f1:
+                        new_line = line.replace('_n1_', n1)
+                        new_line = new_line.replace('_n2_', n1)
+                        new_line = new_line.replace('_n3_', n1)
+                        new_line = new_line.replace('_tmax_', tmax)
+                        new_line = new_line.replace('_ntry_', ntry)
+                        new_line = new_line.replace('_fdOrder_', fdOrder)
+                        new_line = new_line.replace('_snapDt_', snapDt)
+                        new_line = new_line.replace('_testMode_', testMode)
+                        new_line = new_line.replace('_propagator_', propagator)
+                        new_line = new_line.replace('_nmode_', nmode)
+                        new_line = new_line.replace('_dt_', dt)
+                        new_line = new_line.replace('_ratioCFL_', ratioCFL)
+                        new_line = new_line.replace('_dim_', dim)
+
+                        f2.write(new_line)
+
+                    # close files
+                    f1.close()
+                    f2.close()
+
+                    ii = ii + 1
+
+# END
---
+++ script/testCase_Propa/strongWeakScalability/hpcscanPropaStrongWeakScalability.sh
@@ -0,0 +1,92 @@
+
+export KMP_AFFINITY=scatter,1,0,granularity=fine 
+export OMP_NUM_THREADS=32 
+
+start_time=$(date)
+
+sh ../../clean_dir.sh
+
+# Weak Scalability
+fdOrder=8
+
+$HPCSCAN_MPI_INVOKER -n 1 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 1 -nsub3 1
+
+$HPCSCAN_MPI_INVOKER -n 2 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10  \
+			-nsub1 1 -nsub2 2 -nsub3 1
+
+$HPCSCAN_MPI_INVOKER -n 4 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 2 -nsub3 2
+
+$HPCSCAN_MPI_INVOKER -n 6 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 3 -nsub3 2
+
+$HPCSCAN_MPI_INVOKER -n 8 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 4 -nsub3 2
+
+# Strong Scalability
+fdOrder=8
+
+$HPCSCAN_MPI_INVOKER -n 1 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 1000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 1 -nsub3 1
+
+$HPCSCAN_MPI_INVOKER -n 2 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 2000 -n3 1000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10  \
+			-nsub1 1 -nsub2 2 -nsub3 1
+
+$HPCSCAN_MPI_INVOKER -n 4 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 2000 -n3 2000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 2 -nsub3 2
+
+$HPCSCAN_MPI_INVOKER -n 6 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 3000 -n3 2000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 3 -nsub3 2
+
+$HPCSCAN_MPI_INVOKER -n 8 ../../../bin/hpcscan \
+			-ntry 3 -testMode CacheBlk -testCase Propa -propagator Ac2Standard \
+			-dim 1 -nt 100 -dt 0.0 -ratioCFL 1.0 \
+			-fdOrder ${fdOrder} -n1 1000 -n2 4000 -n3 2000 \
+			-param1 130 -param2 130 -param3 130 -snapInc 10 \
+			-nsub1 1 -nsub2 4 -nsub3 2
+
+end_time=$(date)
+
+echo '*** TEST ENDED ***'
+echo "# Started  : " $start_time 
+echo "# Ended    : " $end_time
+echo "On machine : " `hostname`
+echo "Done by    : " `whoami`
+
