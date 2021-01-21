a537e0c14ac40076f50c485a46bd6a237f16331c
vincent <vetienne@rocketmail.com>
2021-01-21 08:49:21 +0300

    update readme

--- src/version_hpcscan.h
+++ src/version_hpcscan.h
@@ -1,4 +1,4 @@
-const char HPCSCAN_GIT_COMMIT[] = "commit a9af76cde0c5f5c93f9aaa21549c8e2e785a58dd" ;
-const char HPCSCAN_GIT_AUTHOR[] = "Author: vincent <vetienne@rocketmail.com>" ;
-const char HPCSCAN_GIT_DATE[] = "Date:   Sun Jan 3 12:20:31 2021 +0300" ;
-const time_t HPCSCAN_COMPILE_DATE = 1609737403 ;
+const char HPCSCAN_GIT_COMMIT[] = "commit c8cf27ab606b1ce9981a38707987cdf1bd5eb6a8" ;
+const char HPCSCAN_GIT_AUTHOR[] = "Author: Vincent Etienne <vincent.etienne@aramco.com>" ;
+const char HPCSCAN_GIT_DATE[] = "Date:   Thu Jan 21 08:06:48 2021 +0300" ;
+const time_t HPCSCAN_COMPILE_DATE = 1611206196 ;
