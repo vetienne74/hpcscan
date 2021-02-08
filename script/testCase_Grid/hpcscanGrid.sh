5a695ed03fd9e4705953b3579c1c55c2c097f57e
vincent <vetienne@rocketmail.com>
2021-02-08 10:23:41 +0300

    update testCase_Grid/hpcscanGrid.sh

--- script/testCase_Grid/hpcscanGrid.sh
+++ script/testCase_Grid/hpcscanGrid.sh
@@ -15,13 +15,13 @@ sh ../clean_dir.sh
 n1=500
 n2=500
 n3=500
-$HPCSCAN_MPI_INVOKER ../../bin/hpcscan -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
 
 # grid size
 n1=1000
 n2=1000
 n3=1000
-$HPCSCAN_MPI_INVOKER ../../bin/hpcscan -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
 
 end_time=$(date)
 
