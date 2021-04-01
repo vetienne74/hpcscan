51a20762b77e6819faef56501439d139fa12dfbe
Vincent Etienne <vincent.etienne@aramco.com>
2021-04-01 09:47:55 +0300

    update hpcscanFD_D2cacheBlkAnalysisPlotData.m

--- script/testCase_FD_D2/cacheBlkAnalysis/hpcscanFD_D2cacheBlkAnalysisPlotData.m
+++ script/testCase_FD_D2/cacheBlkAnalysis/hpcscanFD_D2cacheBlkAnalysisPlotData.m
@@ -8,6 +8,7 @@ DIR  = '.' ;
 % log file name with .log extension
 %FILE = 'hpcscanFD_D2Shaheen' ;
 FILE = 'hpcscan.perf.FD_D2' ;
+FILE = 'hpcscanFD_D2cacheBlkAnalysisNECPlconec01' ;
 
 % indicate below the range of parameters in the log file
 fdOrderInLog = [2,4,8,12,16] ;
@@ -16,7 +17,8 @@ cb2InLog     = 1:32 ;
 cb3InLog     = 1:32 ;
 
 PLOT_GPOINT = 0 ;
-PLOT_GFLOP   = 1 ;
+PLOT_GFLOP  = 0 ;
+PLOT_GBYTE  = 1 ;
 % end input parameters
 %--------------------------------------------------------------------------
 
@@ -27,12 +29,16 @@ ncb2InLog     = numel(cb2InLog)
 ncb3InLog     = numel(cb3InLog)
 perfMapD2Axis1Gpoint = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2Axis1Gflop  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
+perfMapD2Axis1Gbyte  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2Axis2Gpoint = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2Axis2Gflop  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
+perfMapD2Axis2Gbyte  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2Axis3Gpoint = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2Axis3Gflop  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
+perfMapD2Axis3Gbyte  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2LaplaGpoint = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 perfMapD2LaplaGflop  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
+perfMapD2LaplaGbyte  = zeros(nfdOrderInLog, ncb1InLog, ncb2InLog, ncb3InLog) ;
 
 % allocate table to store max val and optimal cache size
 maxD2Axis1_gpoint = zeros(nfdOrderInLog,1) ;
@@ -43,6 +49,10 @@ maxD2Axis1_gflop = zeros(nfdOrderInLog,1) ;
 maxD2Axis2_gflop = zeros(nfdOrderInLog,1) ;
 maxD2Axis3_gflop = zeros(nfdOrderInLog,1) ;
 maxD2Lapla_gflop = zeros(nfdOrderInLog,1) ;
+maxD2Axis1_gbyte = zeros(nfdOrderInLog,1) ;
+maxD2Axis2_gbyte = zeros(nfdOrderInLog,1) ;
+maxD2Axis3_gbyte = zeros(nfdOrderInLog,1) ;
+maxD2Lapla_gbyte = zeros(nfdOrderInLog,1) ;
 maxD2Axis1_cb = zeros(nfdOrderInLog,3) ;
 maxD2Axis2_cb = zeros(nfdOrderInLog,3) ;
 maxD2Axis3_cb = zeros(nfdOrderInLog,3) ;
@@ -105,6 +115,12 @@ for iconfig = 1:sizeLog(1)
     perfMapD2Axis2Gpoint(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2Axis2GpointFD) ;
     perfMapD2Axis3Gpoint(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2Axis3GpointFD) ;
     perfMapD2LaplaGpoint(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2LaplaGpointFD) ;
+    
+    % store GB
+    perfMapD2Axis1Gbyte(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2Axis1GB) ;
+    perfMapD2Axis2Gbyte(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2Axis2GB) ;
+    perfMapD2Axis3Gbyte(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2Axis3GB) ;
+    perfMapD2LaplaGbyte(idxFdOrder, idxCb1, idxCb2, idxCb3) = val.data(iconfig,iD2LaplaGB) ;
 end
 
 disp('START FIND OPTIM CACHE SIZE...')
@@ -115,6 +131,7 @@ for iOrder = 1:nfdOrderInLog
                 if perfMapD2Axis1Gpoint(iOrder, iCb1, iCb2, iCb3) > maxD2Axis1_gpoint(iOrder)
                     maxD2Axis1_gpoint(iOrder) = perfMapD2Axis1Gpoint(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Axis1_gflop(iOrder) = perfMapD2Axis1Gflop(iOrder, iCb1, iCb2, iCb3) ;
+                    maxD2Axis1_gbyte(iOrder) = perfMapD2Axis1Gbyte(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Axis1_cb(iOrder, 1) = iCb1 ;
                     maxD2Axis1_cb(iOrder, 2) = iCb2 ;
                     maxD2Axis1_cb(iOrder, 3) = iCb3 ;
@@ -122,6 +139,7 @@ for iOrder = 1:nfdOrderInLog
                 if perfMapD2Axis2Gpoint(iOrder, iCb1, iCb2, iCb3) > maxD2Axis2_gpoint(iOrder)
                     maxD2Axis2_gpoint(iOrder) = perfMapD2Axis2Gpoint(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Axis2_gflop(iOrder) = perfMapD2Axis2Gflop(iOrder, iCb1, iCb2, iCb3) ;
+                    maxD2Axis2_gbyte(iOrder) = perfMapD2Axis2Gbyte(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Axis2_cb(iOrder, 1) = iCb1 ;
                     maxD2Axis2_cb(iOrder, 2) = iCb2 ;
                     maxD2Axis2_cb(iOrder, 3) = iCb3 ;
@@ -129,6 +147,7 @@ for iOrder = 1:nfdOrderInLog
                 if perfMapD2Axis3Gpoint(iOrder, iCb1, iCb2, iCb3) > maxD2Axis3_gpoint(iOrder)
                     maxD2Axis3_gpoint(iOrder) = perfMapD2Axis3Gpoint(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Axis3_gflop(iOrder) = perfMapD2Axis3Gflop(iOrder, iCb1, iCb2, iCb3) ;
+                    maxD2Axis3_gbyte(iOrder) = perfMapD2Axis3Gbyte(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Axis3_cb(iOrder, 1) = iCb1 ;
                     maxD2Axis3_cb(iOrder, 2) = iCb2 ;
                     maxD2Axis3_cb(iOrder, 3) = iCb3 ;
@@ -136,6 +155,7 @@ for iOrder = 1:nfdOrderInLog
                 if perfMapD2LaplaGpoint(iOrder, iCb1, iCb2, iCb3) > maxD2Lapla_gpoint(iOrder)
                     maxD2Lapla_gpoint(iOrder) = perfMapD2LaplaGpoint(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Lapla_gflop(iOrder) = perfMapD2LaplaGflop(iOrder, iCb1, iCb2, iCb3) ;
+                    maxD2Lapla_gbyte(iOrder) = perfMapD2LaplaGbyte(iOrder, iCb1, iCb2, iCb3) ;
                     maxD2Lapla_cb(iOrder, 1) = iCb1 ;
                     maxD2Lapla_cb(iOrder, 2) = iCb2 ;
                     maxD2Lapla_cb(iOrder, 3) = iCb3 ;
@@ -147,7 +167,7 @@ end
 
 
 for iOrder = 1:nfdOrderInLog
-%for iOrder = 5:5
+    %for iOrder = 5:5
     
     if (PLOT_GFLOP)
         %Gflop
@@ -230,6 +250,48 @@ for iOrder = 1:nfdOrderInLog
         colormap(jet)
         
         figFile = sprintf('%s_GpointFD_O%d.jpg', FILE, fdOrderInLog(iOrder)) ;
+        print('-djpeg', figFile)
+    end
+    
+    %Gbyte
+    if (PLOT_GBYTE)
+        figure('Position', [100 100 1000 800'])
+        
+        subplot(2,2,1); hold on
+        title(sprintf('FD O%d D2Axis1 Gbyte \n Max %.1f / cb2=%d cb3=%d', ...
+            fdOrderInLog(iOrder), maxD2Axis1_gbyte(iOrder), maxD2Axis1_cb(iOrder, 2), maxD2Axis1_cb(iOrder, 3)))
+        xlabel('block size axis3'); ylabel('block size axis2');
+        perfMap = reshape(perfMapD2Axis1Gbyte(iOrder, 1, :, :), [ncb2InLog ncb3InLog]) ;
+        surf(perfMap); colorbar
+        view(0,90); axis tight
+        
+        subplot(2,2,2); hold on
+        title(sprintf('FD O%d D2Axis2 Gbyte \n Max %.1f / cb2=%d cb3=%d', ...
+            fdOrderInLog(iOrder), maxD2Axis2_gbyte(iOrder), maxD2Axis2_cb(iOrder, 2), maxD2Axis2_cb(iOrder, 3)))
+        xlabel('block size axis3'); ylabel('block size axis2');
+        perfMap = reshape(perfMapD2Axis2Gbyte(iOrder, 1, :, :), [ncb2InLog ncb3InLog]) ;
+        surf(perfMap); colorbar
+        view(0,90); axis tight
+        
+        subplot(2,2,3); hold on
+        title(sprintf('FD O%d D2Axis3 Gbyte \n Max %.1f / cb2=%d cb3=%d', ...
+            fdOrderInLog(iOrder), maxD2Axis3_gbyte(iOrder), maxD2Axis3_cb(iOrder, 2), maxD2Axis3_cb(iOrder, 3)))
+        xlabel('block size axis3'); ylabel('block size axis2');
+        perfMap = reshape(perfMapD2Axis3Gbyte(iOrder, 1, :, :), [ncb2InLog ncb3InLog]) ;
+        surf(perfMap); colorbar
+        view(0,90); axis tight
+        
+        subplot(2,2,4); hold on
+        title(sprintf('FD O%d Lapla Gbyte \n Max %.1f / cb2=%d cb3=%d', ...
+            fdOrderInLog(iOrder), maxD2Lapla_gbyte(iOrder), maxD2Lapla_cb(iOrder, 2), maxD2Lapla_cb(iOrder, 3)))
+        xlabel('block size axis3'); ylabel('block size axis2');
+        perfMap = reshape(perfMapD2LaplaGbyte(iOrder, 1, :, :), [ncb2InLog ncb3InLog]) ;
+        surf(perfMap); colorbar
+        view(0,90); axis tight
+        
+        colormap(jet)
+        
+        figFile = sprintf('%s_GbyteFD_O%d.jpg', FILE, fdOrderInLog(iOrder)) ;
         print('-djpeg', figFile)
     end
 end
