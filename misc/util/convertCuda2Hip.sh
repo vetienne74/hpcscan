3da232dcb7ad36476e7844b07ffd9d46f98fb691
Vincent Etienne <vetienne@rocketmail.com>
2021-05-05 11:20:56 +0200

    update grid_Hip.hip

--- misc/util/convertCuda2Hip.sh
+++ misc/util/convertCuda2Hip.sh
@@ -1,13 +1,16 @@
 
+# convert grid_Cuda.cu to grid_Hip.hip
 cp ../../src/grid_Cuda.cu .
-
 hipconvertinplace-perl.sh grid_Cuda.cu
-
 sed -i -e 's/Cuda/Hip/g' grid_Cuda.cu
-
 sed -i -e 's/CUDA/HIP/g' grid_Cuda.cu
-
 mv grid_Cuda.cu ../../src/grid_Hip.hip
-
 rm grid_Cuda.cu.prehip
+
+# convert grid_Cuda.h to grid_Hip.h
+cp ../../src/grid_Cuda.h .
+sed -i -e 's/Cuda/Hip/g' grid_Cuda.h
+sed -i -e 's/CUDA/HIP/g' grid_Cuda.h
+mv grid_Cuda.h ../../src/grid_Hip.h
+
 
