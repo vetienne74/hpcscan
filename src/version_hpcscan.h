cb2a97cd30a3a74646294d95d559b50670492146
vincent <vetienne@rocketmail.com>
2021-05-11 07:55:30 +0300

    update version_hpcscan.h

--- src/version_hpcscan.h
+++ src/version_hpcscan.h
@@ -1,4 +1,4 @@
-const char HPCSCAN_GIT_COMMIT[] = "commit 3a39adf29a1d7606f131db5a07258b0f7cb20d71" ;
+const char HPCSCAN_GIT_COMMIT[] = "commit 5d05ff4d818e06599e2f7f86ec9eaa117a3c5ddb" ;
 const char HPCSCAN_GIT_AUTHOR[] = "Author: vincent <vetienne@rocketmail.com>" ;
-const char HPCSCAN_GIT_DATE[] = "Date:   Mon May 10 20:48:17 2021 +0300" ;
-const time_t HPCSCAN_COMPILE_DATE = 1620706893 ;
+const char HPCSCAN_GIT_DATE[] = "Date:   Tue May 11 07:40:42 2021 +0300" ;
+const time_t HPCSCAN_COMPILE_DATE = 1620708684 ;
