d310f18ce6b18e3a6f3bd85dd70d96aa8eb3c1fb
vincent <vetienne@rocketmail.com>
2021-05-13 06:23:32 +0300

    update file for readme

--- src/version_hpcscan.h
+++ src/version_hpcscan.h
@@ -1,4 +1,4 @@
-const char HPCSCAN_GIT_COMMIT[] = "commit 86941a656981e047c0bd18c98204f8f8210e1fdc" ;
+const char HPCSCAN_GIT_COMMIT[] = "commit 675d8396542ce2fad84b0b8915492d86e8de5998" ;
 const char HPCSCAN_GIT_AUTHOR[] = "Author: vincent <vetienne@rocketmail.com>" ;
-const char HPCSCAN_GIT_DATE[] = "Date:   Thu May 13 05:48:54 2021 +0300" ;
-const time_t HPCSCAN_COMPILE_DATE = 1620875049 ;
+const char HPCSCAN_GIT_DATE[] = "Date:   Thu May 13 06:07:19 2021 +0300" ;
+const time_t HPCSCAN_COMPILE_DATE = 1620876032 ;
