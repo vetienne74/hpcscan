7bd8f2c57af8a1587e7d24e46b6b7cdc4c587cba
vincent <vetienne@rocketmail.com>
2021-05-18 10:50:34 +0200

    add setEnvIcx2IntelOneAPI_dpcpp.sh

--- env/setEnvIcx2IntelOneAPI_icpx.sh
+++ env/setEnvIcx2IntelOneAPI_icpx.sh
@@ -9,15 +9,16 @@ source /raid/opt/intel/oneapi/setvars.sh
 export HPCSCAN_MPI_INVOKER='mpirun'
 
 # OpenMP config
-export HPCSCAN_NTHREADS=72
+export HPCSCAN_NTHREADS=64
 export KMP_AFFINITY=granularity=fine,compact
 export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
 
 # C++ compiler
-export HPCSCAN_CPP=mpiicpc
+export HPCSCAN_CPP='mpiicpc -cxx=icpx'
 export HPCSCAN_CPP_OPENACC_FLAG=
 #export HPCSCAN_CPP_FLAGCOMP='-w -g -O3 -fopenmp -std=c++11 -xHost -ffast-math -fpermissive'
-export HPCSCAN_CPP_FLAGCOMP='-xICELAKE-SERVER -w -g -O3 -fopenmp -std=c++11 -qopt-zmm-usage=high -no-prec-div -no-prec-sqrt -fno-fnalias -fno-nalias -ftz -fma -fp-model fast=2  -qopt-dynamic-alig -fimf-use-svml=true -fimf-precision -qopt-report=3 -qopt-report-annotate=text -qopt-report-phase=vecq -fpermissive'
+export HPCSCAN_CPP_FLAGCOMP='-w -g -O3 -fopenmp -std=c++11 -xHost -ffast-math'
+#export HPCSCAN_CPP_FLAGCOMP='-xICELAKE-SERVER -w -g -O3 -fopenmp -std=c++11 -qopt-zmm-usage=high -no-prec-div -no-prec-sqrt -fno-fnalias -fno-nalias -ftz -fma -fp-model fast=2  -qopt-dynamic-alig -fimf-use-svml=true -fimf-precision -qopt-report=3 -qopt-report-annotate=text -qopt-report-phase=vecq -fpermissive'
 export HPCSCAN_CPP_LIB=
 
 # CUDA compiler
