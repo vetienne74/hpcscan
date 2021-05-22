e12672f200bd5bd45b14e4002f96829cc7bf44d8
vincent <vetienne@rocketmail.com>
2021-05-22 09:59:05 +0300

    add setEnvNeptuneOneAPI_dpcpp.sh

--- src/version_hpcscan.h
+++ src/version_hpcscan.h
@@ -1,4 +1,4 @@
-const char HPCSCAN_GIT_COMMIT[] = "commit 3b42db0a49014056bd9fbbf2f4d0ff8a6a2622aa" ;
+const char HPCSCAN_GIT_COMMIT[] = "commit 7bd8f2c57af8a1587e7d24e46b6b7cdc4c587cba" ;
 const char HPCSCAN_GIT_AUTHOR[] = "Author: vincent <vetienne@rocketmail.com>" ;
-const char HPCSCAN_GIT_DATE[] = "Date:   Sat May 15 06:18:42 2021 +0300" ;
-const time_t HPCSCAN_COMPILE_DATE = 1621064171 ;
+const char HPCSCAN_GIT_DATE[] = "Date:   Tue May 18 10:50:34 2021 +0200" ;
+const time_t HPCSCAN_COMPILE_DATE = 1621666496 ;
