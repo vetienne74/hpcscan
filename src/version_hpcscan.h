785e9f3998b3b3c95cf98a5d8b71b32eef365ef7
vincent <vetienne@rocketmail.com>
2021-06-06 13:57:32 +0300

    added misc/streamReference

--- src/version_hpcscan.h
+++ src/version_hpcscan.h
@@ -1,4 +1,4 @@
-const char HPCSCAN_GIT_COMMIT[] = "commit a69ef5122324cf38fd41af8047456350a9f27c74" ;
+const char HPCSCAN_GIT_COMMIT[] = "commit c9ceb8469c163d9712209c85e1ba1a0f4d6b94f0" ;
 const char HPCSCAN_GIT_AUTHOR[] = "Author: vincent <vetienne@rocketmail.com>" ;
-const char HPCSCAN_GIT_DATE[] = "Date:   Sat Jun 5 06:33:10 2021 +0300" ;
-const time_t HPCSCAN_COMPILE_DATE = 1622867868 ;
+const char HPCSCAN_GIT_DATE[] = "Date:   Sat Jun 5 07:40:04 2021 +0300" ;
+const time_t HPCSCAN_COMPILE_DATE = 1622955052 ;
