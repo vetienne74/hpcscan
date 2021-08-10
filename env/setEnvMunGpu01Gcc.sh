31f0b61fe07a11034e76b0e788e3e944c6ce81f5
Vincent Etienne <vetienne@rocketmail.com>
2021-08-09 23:30:32 -0700

    update env script to use mvapich2

--- env/setEnvMunGpu01Gcc.sh
+++ env/setEnvMunGpu01Gcc.sh
@@ -2,10 +2,12 @@
 # Load needed modules
 # no module needed on MunGpu01
 
-export PATH=/opt/mpi/ompi/bin:$PATH
+#export PATH=/opt/mpi/ompi/bin:$PATH
+export PATH=/opt/mvapich2/gdr/2.3.5/mcast/no-openacc/rocm4.1.1/mofed5.0/mpirun/gnu8.3.0/bin/:$PATH
 
 # MPI config
-export HPCSCAN_MPI_INVOKER='mpirun --oversubscribe'
+#export HPCSCAN_MPI_INVOKER='mpirun --oversubscribe'
+export HPCSCAN_MPI_INVOKER='mpirun'
 
 # OpenMP config
 export HPCSCAN_NTHREADS=128
@@ -16,7 +18,7 @@ export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
 export HPCSCAN_CPP=mpic++
 export HPCSCAN_CPP_OPENACC_FLAG=
 export HPCSCAN_CPP_FLAGCOMP='-g -O3 -mavx2 -fopenmp'
-export HPCSCAN_CPP_LIB=
+export HPCSCAN_CPP_LIB='-L/opt/rocm/lib -lamdhip64'
 
 # CUDA compiler
 export HPCSCAN_CUDA=
--- env/setEnvMunGpu01GccHip.sh
+++ env/setEnvMunGpu01GccHip.sh
@@ -2,10 +2,12 @@
 # Load needed modules
 # no module needed on MunGpu01
 
-export PATH=/opt/mpi/ompi/bin:$PATH
+#export PATH=/opt/mpi/ompi/bin:$PATH
+export PATH=/opt/mvapich2/gdr/2.3.5/mcast/no-openacc/rocm4.1.1/mofed5.0/mpirun/gnu8.3.0/bin/:$PATH
 
 # MPI config
-export HPCSCAN_MPI_INVOKER='mpirun --oversubscribe'
+#export HPCSCAN_MPI_INVOKER='mpirun --oversubscribe'
+export HPCSCAN_MPI_INVOKER='mpirun'
 
 # OpenMP config
 export HPCSCAN_NTHREADS=128
@@ -16,7 +18,7 @@ export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
 export HPCSCAN_CPP=mpic++
 export HPCSCAN_CPP_OPENACC_FLAG=
 export HPCSCAN_CPP_FLAGCOMP='-g -O3 -mavx2 -fopenmp -fPIC'
-export HPCSCAN_CPP_LIB=
+export HPCSCAN_CPP_LIB='-L/opt/rocm/lib -lamdhip64'
 
 # CUDA compiler
 export HPCSCAN_CUDA=
@@ -25,7 +27,8 @@ export HPCSCAN_CUDA_LIB=
 
 # HIP compiler
 export HPCSCAN_HIP=hipcc
-export HPCSCAN_HIP_FLAGCOMP='-fPIC -I /opt/mpi/ompi/include/'
+#export HPCSCAN_HIP_FLAGCOMP='-fPIC -I /opt/mpi/ompi/include/'
+export HPCSCAN_HIP_FLAGCOMP='-fPIC -I /opt/mvapich2/gdr/2.3.5/mcast/no-openacc/rocm4.1.1/mofed5.0/mpirun/gnu8.3.0/include/'
 export HPCSCAN_HIP_LIB='-L/opt/rocm/lib -lamdhip64 '
 
 # display Hpcscan settings
