e2518d2dad7c0e029cc22dcde200c0133c23d0ca
vincent <vetienne@rocketmail.com>
2022-12-22 14:20:18 +0100

    updated README

---
+++ env/setEnvNeptuneGcc.sh
@@ -0,0 +1,30 @@
+
+# Load needed modules
+
+# MPI config
+export HPCSCAN_MPI_INVOKER='mpirun --oversubscribe'
+
+# OpenMP config
+export HPCSCAN_NTHREADS=2
+export KMP_AFFINITY=granularity=fine,compact
+export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
+
+# C++ compiler
+export HPCSCAN_CPP=mpic++
+export HPCSCAN_CPP_OPENACC_FLAG=
+export HPCSCAN_CPP_FLAGCOMP='-g -O3 -mavx2 -fopenmp'
+export HPCSCAN_CPP_LIB=
+
+# CUDA compiler
+export HPCSCAN_CUDA=
+export HPCSCAN_CUDA_FLAGCOMP=
+export HPCSCAN_CUDA_LIB=
+
+# HIP compiler
+export HPCSCAN_HIP=
+export HPCSCAN_HIP_FLAGCOMP=
+export HPCSCAN_HIP_LIB=
+
+# display Hpcscan settings
+sh ./displayHpcscanEnv.sh
+
