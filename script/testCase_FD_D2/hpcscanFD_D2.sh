12d2f1dac676c2c9d0b926e728a0e413e9a1812c
Vincent <vincent.etienne@emea.nec.com>
2022-12-23 15:27:07 +0100

    updated testCase_FD_D2

--- script/testCase_FD_D2/hpcscanFD_D2.sh
+++ script/testCase_FD_D2/hpcscanFD_D2.sh
@@ -21,13 +21,31 @@ $HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode 
 $HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 12
 $HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 14
 $HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 16
+mv hpcscan.perf.FD_D2.log hpcscanPerfFD_D2${testMode}.log
 
-# Same with CacheBlk
-#$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 2
-#$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 4
-#$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 8
-#$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 12
-#$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode CacheBlk -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 16
+# FD_D2 NEC
+testMode='NEC'
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 2 -autoPad -n1Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 4 -autoPad 
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 6 -autoPad -n1Offset 1 -n2Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 8 -autoPad
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 10 -autoPad -n1Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 12 -autoPad
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 14 -autoPad -n1Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 16 -autoPad
+mv hpcscan.perf.FD_D2.log hpcscanPerfFD_D2${testMode}.log
+
+# FD_D2 NEC_SCA
+testMode='NEC_SCA'
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 2 -autoPad -n1Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 4 -autoPad
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 6 -autoPad -n1Offset 1 -n2Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 8 -autoPad
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 10 -autoPad -n1Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 12 -autoPad
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 14 -autoPad -n1Offset 1
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase FD_D2 -testMode $testMode -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3} -fdOrder 16 -autoPad
+mv hpcscan.perf.FD_D2.log hpcscanPerfFD_D2${testMode}.log
 
 end_time=$(date)
 
