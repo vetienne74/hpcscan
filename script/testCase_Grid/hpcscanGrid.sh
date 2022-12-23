24d60749e40e40c055af064d38af8dea0639f44e
Vincent <vincent.etienne@emea.nec.com>
2022-12-23 12:37:21 +0100

    updated testCase_Grid

--- script/testCase_Grid/hpcscanGrid.sh
+++ script/testCase_Grid/hpcscanGrid.sh
@@ -3,6 +3,8 @@ export KMP_AFFINITY=granularity=fine,compact
 export OMP_NUM_THREADS=$HPCSCAN_NTHREADS
 
 start_time=$(date)
+testMode=Baseline
+#testMode=NEC
 
 sh ../clean_dir.sh
 
@@ -10,13 +12,13 @@ sh ../clean_dir.sh
 n1=500
 n2=500
 n3=500
-$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testMode $testMode -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
 
 # grid size
 n1=1000
 n2=1000
 n3=1000
-$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
+$HPCSCAN_MPI_INVOKER -n 1 ../../bin/hpcscan -testMode $testMode -testCase Grid -dim 3 -n1 ${n1} -n2 ${n2} -n3 ${n3}
 
 end_time=$(date)
 
---
+++ script/testCase_Grid/hpcscanGridPlotData.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#data = pd.read_csv("hpcscan.perf.Grid.log", sep=" ", header=None)
+#data = pd.read_csv("hpcscanGridShaheen.log", sep=" ", header=None)
+data = pd.read_csv("hpcscanGridProto27.log", sep=" ", header=None)
+
+print(data.shape)
+
+y_names = []
+y_names.append('Fill')
+y_names.append('MaxErr')
+y_names.append('L1Err')
+y_names.append('SumAbs')
+y_names.append('SumAbsDiff')
+y_names.append('Max')
+y_names.append('Min')
+y_names.append('UptPressure')
+
+fig, axis = plt.subplots(1,2,figsize=(14,6))
+
+all_y = []
+j=0
+for i in range(13,28+1,2):  
+    all_y.append(((data[i].tolist()),y_names[j]))
+    j+=1
+
+for i in range(8):
+        line1 = axis[0].bar(all_y[i][1],height=all_y[i][0][0], color='steelblue', width = -0.3,align='edge')
+        line2 = axis[0].bar(all_y[i][1],height=all_y[i][0][1], color='firebrick', width = 0.3, align='edge')
+
+line1.set_label('500x500x500') #legend contains only these 2
+line2.set_label('1000x1000x1000') #legend contains only these 2
+axis[0].set_ylabel('GByte/s')
+axis[0].set_title('Test Case Grid')
+axis[0].legend()
+axis[0].grid()
+axis[0].set_axisbelow(True) # to make the grid lines go behind
+
+
+## Next image
+
+all_y = []
+j=0
+for i in range(14,29+1,2):
+    all_y.append(((data[i].tolist()),y_names[j]))
+    j+=1
+
+for i in range(8):
+    line1 =axis[1].bar(all_y[i][1],height=all_y[i][0][0], color='steelblue', width = -0.3,align='edge')
+    line2 =axis[1].bar(all_y[i][1],height=all_y[i][0][1], color='firebrick', width = 0.3,align='edge')  #linewidth=1, edgecolor='black'
+    
+line1.set_label('500x500x500') #legend contains only these 2
+line2.set_label('1000x1000x1000') #legend contains only these 2
+axis[1].set_ylabel('GPoint/s')
+axis[1].set_title('Test Case Grid')
+axis[1].legend()
+axis[1].grid()
+axis[1].set_axisbelow(True) # to make the grid lines go behind
+
+
+plt.tight_layout()
+plt.savefig("hpcscanGridPlotData.png")
+plt.show()
+plt.close()
