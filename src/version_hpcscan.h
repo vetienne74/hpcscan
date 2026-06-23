23303395d7d6fa7f5645dff0dd89366ac831306e
vincent <vincent.etienne@algodoers.com>
2026-06-23 12:04:46 +0200

    added FD_D1 tests in validation test driver

--- src/version_hpcscan.h
+++ src/version_hpcscan.h
@@ -1,4 +1,4 @@
-const char HPCSCAN_GIT_COMMIT[] = "commit bc5a4c9d1ed6ca64f33c9933f54b3a6abc823af8" ;
+const char HPCSCAN_GIT_COMMIT[] = "commit 5a7377993b74972ef44a9de36744a3526a0a227a" ;
 const char HPCSCAN_GIT_AUTHOR[] = "Author: vincent <vincent.etienne@algodoers.com>" ;
-const char HPCSCAN_GIT_DATE[] = "Date:   Tue Jun 23 10:09:51 2026 +0200" ;
-const time_t HPCSCAN_COMPILE_DATE = 1782204928 ;
+const char HPCSCAN_GIT_DATE[] = "Date:   Tue Jun 23 11:15:48 2026 +0200" ;
+const time_t HPCSCAN_COMPILE_DATE = 1782206348 ;
